c9f65585c4d60b65bb020dfae5f84e17adf8a3d7
Afeez Adewumi <adewumilanre01@gmail.com>
2022-12-02 17:31:26 -0600

    Adding Booking.com API Best hotel search by city and country

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '46acffddd6msh2dd0013c039590dp128e31jsnbab1cf5c867a',
+      'X-RapidAPI-Host': 'best-booking-com-hotel.p.rapidapi.com'
+    }
+  };
+
+  const cityName = document.querySelector('#city').value
+  const countryName = document.querySelector('#country').value
+  console.log(city)
+  console.log(country)
+
+fetch(`https://best-booking-com-hotel.p.rapidapi.com/booking/best-accommodation?cityName=${cityName}&countryName=${countryName}`, options)
+	.then(response => response.json())
+	.then(response => {
+      console.log(response)
+      document.querySelector('h2').innerText = response.name
+      document.querySelector("a").innerHTML = response.link
+      document.querySelector('h3').innerText = response.rating
+  })
+
+
+	.catch(err => console.error(err));
+}
+
+
+  
+
